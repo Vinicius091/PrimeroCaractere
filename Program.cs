d4924d7cec76c1a1d0ccd4994f3b1fa9736c2360
Vinicius091 <viniferr2006@gmail.com>
2026-09-09 22:42:29 -0300

    Exibe o primeiro caracter

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main ()
+    {
+        //Lê a linha digitada pelo usuario
+        string entrada = Console.ReadLine();
+
+        //Remove os espaços do início e do fim
+        string textoSemEspaços = entrada.Trim();
+
+        // Pega o primeiro caractere da string limpa
+        char primeiroCaractere = textoSemEspacos[0];
+
+        //exibe o resultado esperado
+        Console.WriteLine(primeiroCaractere);
+
+    }
+
+}
